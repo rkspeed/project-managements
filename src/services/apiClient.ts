22045d3e81043d2904eb0a9f0ee91963833272f4
rkspeed <rasikadevi94@gmail.com>
2025-12-01 14:22:43 +0530

    init mock api

---
+++ src/services/apiClient.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:4000', // json-server or MSW
+  timeout: 10000
+});
+
+export default api;
